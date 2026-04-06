190a1dee9e1da6a858ad9547e706324d52f8ba39
Deise Domingues <dominguesdeise@gmail.com>
2026-04-06 13:21:11 -0300

    feat: cria telas de menu cadastro e menu edições

---
+++ js/menu-edicoes.js
@@ -0,0 +1,25 @@
+import { supabase } from "./supabase.js";
+import { exigirAdmin } from "./guard.js";
+
+/*
+  Somente admin pode acessar esta tela.
+*/
+await exigirAdmin();
+
+const btnSair = document.getElementById("btnSair");
+
+btnSair.addEventListener("click", async () => {
+  try {
+    await supabase.auth.signOut();
+
+    localStorage.removeItem("role");
+    localStorage.removeItem("professorId");
+    localStorage.removeItem("professorNome");
+    localStorage.removeItem("professorEmail");
+
+    window.location.href = "index.html";
+  } catch (error) {
+    console.error("Erro ao sair:", error);
+    alert("Não foi possível sair neste momento.");
+  }
+});
